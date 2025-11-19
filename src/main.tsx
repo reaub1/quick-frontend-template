e5f1da8019671a43c3ad3867e67158bbfd0992b5
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2025-11-19 22:05:18 +0100

    Added MUI and themes

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,58 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+
+const lightThemeAvailable = true;
+const darkThemeAvailable = true;
+
+interface CustomPaletteColor {
+  accent: string;
+}
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    custom?: CustomPaletteColor;
+  }
+
+  interface PaletteOptions {
+    custom?: CustomPaletteColor;
+  }
+}
+
+const theme = createTheme({
+  colorSchemes: {
+    light: lightThemeAvailable && {
+      palette: {
+        primary: {
+          main: "#00ff00",
+        },
+        custom: {
+          accent: "#ff0000"
+        },
+      },
+    },
+    dark: darkThemeAvailable && {
+      palette: {
+        primary: {
+          main: "#0000ff",
+        },
+        custom: {
+          accent: "#ff0000"
+        },
+      },
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
